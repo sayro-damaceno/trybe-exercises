1d068a53879d8f0b79ea5e274a65aeaa68dba2be
Sayro <sayrodamaceno70@gmail.com>
2021-07-28 19:17:47 -0300

    adiciona ex07

--- fundamentos/bloco-04-introdução-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex06.js
+++ fundamentos/bloco-04-introdução-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex06.js
@@ -1,18 +1,27 @@
-let xadrez = "CAVALO";
+let xadrez = "peãoo";
 xadrezPeca = xadrez.toLowerCase()
 
-if (xadrezPeca === "rei") {
-  console.log("O rei pode mover-se em todas as direções (horizontal, vertical e diagonal) somente uma casa de cada vez.")
-}
-else if (xadrezPeca === "rainha") {
-  console.log("A rainha move-se ao longo da horizontal, vertical e diagonais mas não pode pular outras peças.")
-}
-else if (xadrezPeca === "bispo") {
-  console.log("O bispo move-se ao longo da diagonal. Não pode pular outras peças.")
-}
-else if (xadrezPeca === "cavalo") {
-  console.log("É a única peça que pode pular as outras. O movimento do cavalo é em forma de “L”, quer dizer, duas casas em sentido horizontal e mais uma na vertical ou vice-versa.")
-}
-else if (xadrezPeca === "peao") {
-  console.log("O peão movimenta-se apenas uma casa para frente e somente captura outras peças na diagonal.")
+switch (xadrezPeca) {
+  case "rei":
+    console.log("O rei pode mover-se em todas as direções (horizontal, vertical e diagonal) somente uma casa de cada vez.")
+    break;
+
+  case "rainha":
+    console.log("A rainha move-se ao longo da horizontal, vertical e diagonais mas não pode pular outras peças.")
+    break;
+
+  case "bispo": 
+    console.log("O bispo move-se ao longo da diagonal. Não pode pular outras peças.")
+    break;
+
+  case "cavalo":
+    console.log("É a única peça que pode pular as outras. O movimento do cavalo é em forma de “L”, quer dizer, duas casas em sentido horizontal e mais uma na vertical ou vice-versa.")
+  break;
+
+  case "peão":
+    console.log("O peão movimenta-se apenas uma casa para frente e somente captura outras peças na diagonal.")
+  break;
+
+  default:
+    console.log("Valor não indentificado")
 }
