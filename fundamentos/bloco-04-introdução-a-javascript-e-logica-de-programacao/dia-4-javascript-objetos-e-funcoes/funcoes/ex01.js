455a25b8a5b6d1945a5d8e0aeb3a6baf886b9e16
Sayro <sayrodamaceno70@gmail.com>
2021-08-05 19:40:04 -0300

    adiciona ex01

---
+++ fundamentos/bloco-04-introdução-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes/ex01.js
@@ -0,0 +1,8 @@
+function reverseString(string) {
+  return string.split('').reverse().join('');
+}
+function polidrome(string) {
+  return string === reverseString(string)
+}
+
+console.log(polidrome('arara'))
