ee476d88c3cf589cd7eae758fd46745496bcb262
Sayro <sayrodamaceno70@gmail.com>
2021-08-05 19:17:13 -0300

    exercicios de fixaçao

---
+++ fundamentos/bloco-04-introdução-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-fixacao/script2.js
@@ -0,0 +1,21 @@
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge' 
+};
+
+for (let index in names) {
+  console.log("Olá " + names[index]);
+}
+
+//
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for (let index in car) {
+  console.log(index + " " + car[index])
+}
