30972f3c6fed017fdc46b1210bd35c865be57d5e
Sayro <sayrodamaceno70@gmail.com>
2021-08-06 12:42:02 -0300

    adiciona ex05

---
+++ fundamentos/bloco-04-introdução-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes/ex05.js
@@ -0,0 +1,19 @@
+function numberRepet(array) {
+  // let array = [1, 4, 8, 5, 8, 2, 3]
+  let cont2 = 0;
+  let num = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    let cont = 0;
+    for (let j = 0; j < array.length; j += 1) {
+      if (array[i] === array[j]) {
+        cont += 1;
+      }
+    }
+    if (cont > cont2) {
+      cont2 = cont;
+      num = array[i];
+    }
+  }
+  return num;
+}
+console.log(numberRepet([1, 4, 8, 5, 8, 2, 3]))
