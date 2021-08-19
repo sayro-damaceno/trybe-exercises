fdf5e5ae716b57fc5522b5eebd77c3945ba6341d
Sayro <sayrodamaceno70@gmail.com>
2021-08-19 02:19:24 -0300

    adiciona exercicios do dia 1 bloco 6

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,38 @@
+const estados = document.getElementById('estados');
+
+let estadosBrasil = [
+  'AC',
+  'AL',
+  'AP',
+  'AM',
+  'BA',
+  'CE',
+  'DF',
+  'ES',
+  'GO',
+  'MA',
+  'MT',
+  'MS',
+  'MG',
+  'PA',
+  'PB',
+  'PR',
+  'PE',
+  'PI',
+  'RJ',
+  'RN',
+  'RS',
+  'RO',
+  'RR',
+  'SC',
+  'SP',
+  'SE',
+  'TO',
+]
+
+for (let index = 0; index < estadosBrasil.length; index += 1) {
+  let option = document.createElement('option');
+  option.innerText = estadosBrasil[index];
+  option.value = estadosBrasil[index]
+  estados.appendChild(option);
+}
