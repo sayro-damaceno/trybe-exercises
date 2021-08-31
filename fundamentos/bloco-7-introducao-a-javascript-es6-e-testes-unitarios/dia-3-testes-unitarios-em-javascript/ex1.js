5b29b2568162fcb04a8a92b2320b895e4cef1b98
Sayro <sayrodamaceno70@gmail.com>
2021-08-31 18:42:38 -0300

    adiciona o exercicio 1 da parte 1

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/ex1.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+// implemente seus testes aqui
+// Teste se o retorno de sum(4, 5) é 9
+const expectedNine = sum(4, 5)
+assert.strictEqual(expectedNine, 9, '4 + 5 é igual a 9')
+
+// Teste se o retorno de sum(0, 0) é 0
+const expectedZero = sum(0, 0)
+assert.strictEqual(expectedZero, 0, '0 + 0 é igual a 0')
+
+// Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+assert.throws(() => {
+  sum(4, '5')
+})
+
+// Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
+assert.throws(() => {
+  sum(4, '5')
+}, /^Error: parameters must be numbers$/)
