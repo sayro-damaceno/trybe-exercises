347386c8d9a18f0b7dcb47b2e802a7d4256374c1
Sayro <sayrodamaceno70@gmail.com>
2021-09-02 18:44:43 -0300

    adiciona o exercise 3 e refatora o exercise 2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercise2.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercise2.js
@@ -1,12 +1,7 @@
 const sorteio = (number, callback) => {
   const randomNumber = Math.floor(Math.random() * 5 + 1);
-  const compareNumbers = callback(number, randomNumber);
 
-  if (compareNumbers) {
-    return "Parabéns você ganhou";
-  } else {
-    return "Tente novamente";
-  }
+  return callback(number, randomNumber) ? "Parabéns você ganhou" : "Tente novamente";
 };
 
 const compare = (n1, n2) => n1 === n2;
